1403dc265e18d38e8efb917403e85e4d7041dc26
ali amar <aliamarr033@gmail.com>
2026-01-19 02:02:05 +0200

    chore: remove .history from version control

--- .history/src/lib/services/occasions.service_20260119002650.ts
+++
@@ -1,9 +0,0 @@
-export async function getOccasions() {
-  const res = await fetch(`${process.env.API}/occasions`, {
-    method: "GET",
-    cache: "no-store",
-  });
-
-  if (!res.ok) throw new Error("Failed to fetch occasions");
-  return res.json();
-}
