8c7054e9dfae9011c67a297b3fa158615aa26a1a
marwan5050 <marwanabdalla2015@gmail.com>
2026-02-12 02:45:28 +0200

    finished ui cart design as guest , finished all functionality , make card infinite scroll , handle summary parallel route

--- src/app/[locale]/cart/_components/cart-data/cart-data.tsx
+++ src/app/[locale]/cart/_components/cart-data/cart-data.tsx
@@ -5,10 +5,19 @@ import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import CartGuestData from "../cart-guest-data/cart-guest-data";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
+import { BrushCleaning } from "lucide-react";
+import { MoveLeft } from "lucide-react";
+import { Link } from "@/i18n/navigation";
+
 
 export default function CartData() {
   const { status } = useSession();
   const [guestCart, setGuestCart] = useState<any[]>([]);
+  const t = useTranslations("cart");
 
   // if user  guest →  read from localStorage
   useEffect(() => {
@@ -34,7 +43,41 @@ export default function CartData() {
     return (
       <div>
         {guestCart.length === 0 ? (
-          <p>No items in cart</p>
+          <div className="w-[50rem] mx-auto my-10 px-4 py-2 rounded-[1rem]">
+            <div className="cart-header  flex justify-between items-center mb-4">
+              <div className="text-5xl text-zinc-800 font-bold ">
+                {t("title")}
+                <span className="text-zinc-500 font-normal text-base ms-2">
+                  {guestCart.length} {t("products")}
+                </span>
+              </div>
+              <div className="clear">
+                <Button disabled className="capitalize">
+                  <BrushCleaning size={20} /> {t("empty")}
+                </Button>
+              </div>
+            </div>
+            <Card>
+              <CardContent>
+                <Image
+                  src="/assets/p0.png"
+                  alt="No products"
+                  width={250}
+                  height={214}
+                  className="mx-auto "
+                />
+                <p className="text-sm font-normal text-zinc-400 ">
+                  {t("empty-cart")}
+                </p>
+              </CardContent>
+            </Card>
+            <div className=" text-start py-6">
+              <Button variant={"destructive"} className="capitalize">
+                <MoveLeft size={20} />
+                <Link href="/">{t("continue-shopping")}</Link>
+              </Button>
+            </div>
+          </div>
         ) : (
           // <p> please login to see cart products </p>
           <CartGuestData />
--- src/app/[locale]/cart/_components/cart-guest-data/cart-guest-data.tsx
+++ src/app/[locale]/cart/_components/cart-guest-data/cart-guest-data.tsx
@@ -1,39 +1,93 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
-import { Plus } from "lucide-react";
-import { Minus } from "lucide-react";
-import { Trash2 } from "lucide-react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect, useRef, useState } from "react";
+import { Plus, Minus, Trash2, BrushCleaning, MoveLeft } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-// import NoProduct from "../../../../../../public/assets/p0.png";
-// import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/navigation";
+import type { InfiniteData } from "@tanstack/react-query";
+import { Input } from "@/components/ui/input";
 
 type GuestCartItem = {
   product: string;
   quantity: number;
 };
 
+type ProductsPage = {
+  products: any[];
+  nextPage?: number;
+};
+
+const PAGE_SIZE = 2;
+
 export default function CartGuestData() {
   const [cartItems, setCartItems] = useState<GuestCartItem[]>([]);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
 
-  //   fetch products details for guest cart items and use promise.all to fetch all products in parallel
-  async function fetchProducts(ids: string[]) {
+  const t = useTranslations("cart");
+
+  // fetch products details for guest cart items
+  async function fetchProducts({ pageParam = 0 }: { pageParam: number }) {
+    const start = pageParam * PAGE_SIZE;
+    const end = start + PAGE_SIZE;
+    const pageIds = ids.slice(start, end);
+
+    // use promise.all to fetch all products in parallel
     const results = await Promise.all(
-      ids.map(async (id) => {
+      pageIds.map(async (id) => {
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_API}/products/${id}`
         );
-        if (!res.ok) throw new Error(`Failed to fetch product ${id}`);
+        if (!res.ok) throw new Error("Failed to fetch product");
         const data = await res.json();
-        console.log("Fetched product data:", data);
         return data.product;
       })
     );
-    return results;
+
+    return {
+      products: results,
+      nextPage: end < ids.length ? pageParam + 1 : undefined,
+    };
   }
 
+  // clear cart
+  function clearCart() {
+    localStorage.removeItem("guest-cart");
+    setCartItems([]);
+  }
+
+  // increase quantity
+  const increaseQuantity = (id: string) => {
+    setCartItems((prev) => {
+      const updated = prev.map((item) =>
+        item.product === id
+          ? { ...item, quantity: (item.quantity ?? 1) + 1 }
+          : item
+      );
+      localStorage.setItem("guest-cart", JSON.stringify(updated));
+      return updated;
+    });
+  };
+
+  // decrease quantity
+  const decreaseQuantity = (id: string) => {
+    setCartItems((prev) =>
+      prev.map((item) =>
+        item.product === id
+          ? { ...item, quantity: Math.max((item.quantity ?? 1) - 1, 1) }
+          : item
+      )
+    );
+  };
+
+  // remove product
+  const removeProduct = (id: string) => {
+    setCartItems((prev) => prev.filter((item) => item.product !== id));
+  };
+
+  // get data from lcoastorage and check it no data
   useEffect(() => {
     const stored = localStorage.getItem("guest-cart");
     if (stored) {
@@ -41,124 +95,173 @@ export default function CartGuestData() {
     }
   }, []);
 
+  // get product by id
   const ids = cartItems.map((item) => item.product);
 
-  const { data, isLoading, isError } = useQuery({
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    isError,
+  } = useInfiniteQuery<
+    ProductsPage,
+    Error,
+    InfiniteData<ProductsPage>,
+    (string | string[])[],
+    number
+  >({
     queryKey: ["guest-cart-products", ids],
-    queryFn: () => fetchProducts(ids),
+    queryFn: ({ pageParam }) => fetchProducts({ pageParam }),
+    initialPageParam: 0,
     enabled: ids.length > 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
   });
+
+  useEffect(() => {
+    if (!loadMoreRef.current || !hasNextPage) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchNextPage();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    observer.observe(loadMoreRef.current);
+    return () => observer.disconnect();
+  }, [fetchNextPage, hasNextPage]);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading products</div>;
 
   return (
-    <div>
-      <h2>Guest Cart</h2>
+    <>
+      <div className="flex justify-between px-20 gap-10">
+        <div className="card w-[48rem]">
+          <div className="cart-header flex justify-between items-center mb-4">
+            <div className="text-5xl text-zinc-800 font-bold">
+              {t("title")}
+              <span className="text-zinc-500 font-normal text-base ms-2">
+                {cartItems.length} {t("products")}
+              </span>
+            </div>
+            <Button variant="light" onClick={clearCart} className="capitalize">
+              <BrushCleaning size={20} /> {t("empty")}
+            </Button>
+          </div>
 
-      {/* this image need to work  */}
-      {/* <Image
-        src="/assets/p0.png"
-        alt="No products"
-        width={800}
-        height={800}
-        className="mx-auto my-20"
-      /> */}
-      <div className=" mt-48 w-3/4 ">
-        <div className="cart-header flex justify-between items-center mb-4">
-          <div> cart 0 </div>
-          <div className="clear">
-            <Button variant={"outline"}> Clear Cart </Button>
+          <Card>
+            <CardContent className="max-h-[32rem] overflow-y-auto hide-scrollbar">
+              {data?.pages.map((page) =>
+                page.products.map((product: any) => {
+                  const cartItem = cartItems.find(
+                    (item) => item.product === product._id
+                  );
+
+                  return (
+                    <div
+                      key={product._id}
+                      className="flex justify-between items-center border-b py-4"
+                    >
+                      <div className="flex items-center gap-4">
+                        <img
+                          src={product.imgCover}
+                          alt={product.title}
+                          width={100}
+                          className="rounded h-32 object-cover"
+                        />
+                        <div className="flex flex-col justify-between h-32">
+                          <div>
+                            <p className="font-semibold text-lg text-maroon-600 capitalize pb-2">
+                              {product.title}
+                            </p>
+                            <p className="font-normal text-base">
+                              ⭐{t("rating")}:
+                              <span className="font-medium">
+                                {product.rateAvg}
+                              </span>
+                              <span className="text-blue-600 font-medium text-base ms-2">
+                                ({product.rateCount} {t("ratings")})
+                              </span>
+                            </p>
+                          </div>
+
+                          <p>
+                            <span className="text-maroon-600 text-sm font-medium me-1">
+                              (x{cartItem?.quantity})
+                            </span>
+                            <span className="font-bold text-2xl">
+                              {product.price * (cartItem?.quantity ?? 1)} EGP
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col justify-between items-end h-32">
+                        <Button
+                          variant="destructive"
+                          className="capitalize "
+                          onClick={() =>
+                            cartItem && removeProduct(cartItem?.product)
+                          }
+                        >
+                          <Trash2 /> {t("remove-product")}
+                        </Button>
+
+                        <div className="flex items-center gap-2 ">
+                          <Button
+                            variant="secondary"
+                            onClick={() =>
+                              cartItem && decreaseQuantity(cartItem?.product)
+                            }
+                            disabled={(cartItem?.quantity ?? 1) <= 1}
+                          >
+                            <Minus size={20} />
+                          </Button>
+                          <Input
+                            type="number"
+                            value={cartItem?.quantity ?? 1}
+                            className="w-24 text-center p-4 border border-zinc-300 rounded-[1rem]"
+                            readOnly
+                          />
+                          <Button
+                            variant="secondary"
+                            onClick={() =>
+                              cartItem && increaseQuantity(cartItem?.product)
+                            }
+                          >
+                            <Plus size={20} />
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+              )}
+
+              {hasNextPage && (
+                <div
+                  ref={loadMoreRef}
+                  className="text-center py-6 text-zinc-500"
+                >
+                  {isFetchingNextPage ? "Loading..." : ""}
+                </div>
+              )}
+            </CardContent>
+          </Card>
+
+          <div className="text-start py-6">
+            <Button variant="destructive" className="capitalize">
+              <MoveLeft size={20} />
+              <Link href="/">{t("continue-shopping")}</Link>
+            </Button>
           </div>
         </div>
-
-        <Card>
-          <CardContent>
-            {data?.map((product: any) => {
-              const cartItem = cartItems.find(
-                (item) => item.product === product._id
-              );
-
-              return (
-                <div
-                  key={product._id}
-                  className="flex justify-between items-center border-b py-4"
-                >
-                  {/*  product info */}
-
-                  <div className="flex items-center gap-4">
-                    <img
-                      src={product.imgCover}
-                      alt={product.title}
-                      width={100}
-                      className="rounded h-32 object-cover"
-                    />
-                    <div className="flex flex-col justify-between h-32">
-                      {/* rating */}
-                      <div className="rating ">
-                        <p className="font-semibold text-lg text-maroon-600 capitalize pb-2">
-                          {product.title}
-                        </p>
-                        <p className="font-normal text-base">
-                          ⭐ Rating:
-                          <span className="font-medium">
-                            {" "}
-                            {product.rateAvg}{" "}
-                          </span>
-                          <span className="text-blue-600 font-medium text-base lowercase ms-2">
-                            ({product.rateCount} ratings)
-                          </span>
-                        </p>
-                      </div>
-
-                      {/* total price */}
-                      <div className="total-price">
-                        <p>
-                          <span className="text-maroon-600 text-sm font-medium me-1">
-                            (x{cartItem?.quantity})
-                          </span>
-                          <span className="font-bold text-2xl">
-                            {product.price * (cartItem?.quantity ?? 1)} EGP
-                          </span>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-
-                  {/*   quantity */}
-                  <div className="flex flex-col justify-between h-32">
-                    <div className="removeItme">
-                      <Button variant={"destructive"}>
-                        <Trash2 /> Remove
-                      </Button>
-                    </div>
-                    <div className="quantity flex items-center gap-2">
-                      <Button
-                        variant={"secondary"}
-                        className="px-4 py-2  rounded"
-                      >
-                        <Minus size={20} />
-                      </Button>
-                      <input
-                        type="number"
-                        value={cartItem?.quantity ?? 1}
-                        readOnly
-                        className="w-12 text-center border rounded"
-                      />
-                      <Button
-                        variant={"secondary"}
-                        className="px-4 py-2  rounded"
-                      >
-                        <Plus size={20} />
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </CardContent>
-        </Card>
       </div>
-    </div>
+    </>
   );
 }
